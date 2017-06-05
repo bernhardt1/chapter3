46abccdef621816460be73f9738732737c25e4c0
bernhardt1 <jason_bernhardt_12@hotmail.com>
2017-06-05 15:44:54 -0700

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -84,4 +84,33 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  test "should follow and unfollow a user" do
+    @user = users(:jason)
+    archer  = users(:archer)
+    assert_not @user.following?(archer)
+    @user.follow(archer)
+    assert @user.following?(archer)
+    assert archer.followers.include?(@user)
+    @user.unfollow(archer)
+    assert_not @user.following?(archer)
+  end
+
+  test "feed should have the right posts" do
+    jason = users(:jason)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert jason.feed.include?(post_following)
+    end
+    # Posts from self
+    jason.microposts.each do |post_self|
+      assert jason.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not jason.feed.include?(post_unfollowed)
+    end
+  end
+
 end
